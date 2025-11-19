326945053f4441b986f9243541238e35631a27f4
Cynnics <kevinnpnm@gmail.com>
2025-11-19 17:17:20 +0100

    Soporte para JWT (Swagger)

--- SmartWarehouseAPI/Program.cs
+++ SmartWarehouseAPI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SmartWarehouseAPI.Data;
 using System.Text;
 
@@ -19,7 +20,41 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SmartWarehouse API",
+        Version = "v1"
+    });
+
+    // 🔐 Habilitar botón "Authorize" en Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Introduce el token con el formato: Bearer {token}"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
 
 // 🔗 Conexión a MySQL
 builder.Services.AddDbContext<AppDbContext>(options =>
